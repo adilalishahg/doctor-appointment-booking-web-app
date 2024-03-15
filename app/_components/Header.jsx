4c14f076bfc52d163fb9f1a4075ed0dff1cc9b38
adilalishahg <adilalishahg@gmail.com>
2024-03-15 12:43:01 -0700

    [Header][Designed]

---
+++ app/_components/Header.jsx
@@ -0,0 +1,29 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+    const Menu = [
+        {id:1,name:'Home',path:'/'},
+        {id:2,name:'Explore',path:'/explore'},
+        {id:3,name:'Contact Us',path:'/'},
+    ]
+  return (
+    <div className='flex items-center justify-between p-4 shadow-sm'>
+        <div className="flex items-center gap-10">
+
+            <Image src='/logo.svg' alt='logo' width={180} height={80}/>
+            <ul className='hidden gap-8 md:flex'>
+                {Menu.map((item,index)=>(
+                    <Link href={item.path} key={index}>
+                    <li className='transition-all ease-in-out cursor-pointer hover:text-primary hover:scale-105'>{item.name}</li></Link>
+                    ))}
+            </ul>
+        </div>
+        <Button>Get Started</Button>
+    </div>
+  )
+}
+
+export default Header
